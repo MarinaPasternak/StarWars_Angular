01c7af2a753a4a7358de1791f59a6cafdabaaec9
Marina Pasternak <256pasternakmarina@gmail.com>
2022-10-25 01:12:35 +0300

    UI: added routes

--- angular-star-wars/src/app/app-routing.module.ts
+++ angular-star-wars/src/app/app-routing.module.ts
@@ -1,8 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { StarWarsPageComponent } from './components/star-wars-page/star-wars-page.component';
 
 const routes: Routes = [
-  { }
+  {
+    path: '',
+    pathMatch: 'full',
+    component: SignUpComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  },
+  {
+    path: 'home',
+    component: StarWarsPageComponent
+  }
 ];
 
 @NgModule({
