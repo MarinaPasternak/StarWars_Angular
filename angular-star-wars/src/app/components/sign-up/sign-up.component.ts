b52ea2f0dae77d672d6612210df900f35f77aee4
Marina Pasternak <256pasternakmarina@gmail.com>
2022-10-26 02:33:23 +0300

    UI: added Sign Up from with validation

--- angular-star-wars/src/app/components/sign-up/sign-up.component.ts
+++ angular-star-wars/src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormGroup, 
+  FormBuilder, 
+  Validators,
+  ValidatorFn,
+  ValidationErrors
+} from '@angular/forms';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
+import { HotToastService } from '@ngneat/hot-toast';
+
+export function passwordsMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password 
+        && confirmPassword 
+        && password !== confirmPassword
+      ) {
+      return { passwordsDontMatch: true };
+    } else {
+      return null;
+    }
+  };
+}
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +33,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.scss']
 })
 export class SignUpComponent implements OnInit {
+  signUpForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private toast: HotToastService,
+    private router: Router,
+  ) {
+    this.signUpForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      confirmPassword: ['', Validators.required]
+    }, {
+      validators: passwordsMatchValidator() 
+    });
+  }
 
   ngOnInit(): void {
+  }
+
+  get email() {
+    return this.signUpForm.get('email');
+  }
+
+  get password() {
+    return this.signUpForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword');
   }
 
 }
